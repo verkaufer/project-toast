8fcec2f277e89ee8d6a7fcdbfbf5ffb347f76be6
david.gunter <david.gunter@agilebits.com>
2023-09-19 08:04:16 -0700

    WIP: Exercise 2
    - message passes through to toast
    - toast can open/close
    - pass through icon using variant

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -1,33 +1,27 @@
 import React from 'react';
 import {
-  AlertOctagon,
-  AlertTriangle,
-  CheckCircle,
-  Info,
   X,
 } from 'react-feather';
 
 import VisuallyHidden from '../VisuallyHidden';
-
 import styles from './Toast.module.css';
 
-const ICONS_BY_VARIANT = {
-  notice: Info,
-  warning: AlertTriangle,
-  success: CheckCircle,
-  error: AlertOctagon,
-};
 
-function Toast() {
+function Toast({className, icon: Icon, handleClose, children}) {
+
+  // I think it would be better if we could use a Type in typescript to limit
+  // the possible values here.
+  const styleVariant = styles[className] || styles.notice;
+
   return (
-    <div className={`${styles.toast} ${styles.notice}`}>
+    <div className={`${styles.toast} ${styleVariant}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <Icon size={24}/>
       </div>
       <p className={styles.content}>
-        16 photos have been uploaded
+        {children}
       </p>
-      <button className={styles.closeButton}>
+      <button className={styles.closeButton} onClick={handleClose}>
         <X size={24} />
         <VisuallyHidden>Dismiss message</VisuallyHidden>
       </button>
--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,81 +1,105 @@
 import React from 'react';
 
+import {
+  AlertOctagon,
+  AlertTriangle,
+  CheckCircle,
+  Info,
+} from "react-feather";
+
 import Button from '../Button';
+import Toast from "../Toast";
 
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
+const ICONS_BY_VARIANT = {
+  notice: Info,
+  warning: AlertTriangle,
+  success: CheckCircle,
+  error: AlertOctagon,
+};
+
 function ToastPlayground() {
 
-    const [toastMsg, setToastMsg] = React.useState("");
-    const [variant, setVariant] = React.useState("notice");
+  const [toastMsg, setToastMsg] = React.useState("");
+  const [variant, setVariant] = React.useState("notice");
+  const [showToast, setShowToast] = React.useState(false);
 
-    const handleVariantChange = (event) => {
-        setVariant(event.target.value);
-    }
+  const handleVariantChange = (event) => {
+    setVariant(event.target.value);
+  }
 
-    return (
-        <div className={styles.wrapper}>
-            <header>
-                <img alt="Cute toast mascot" src="/toast.png"/>
-                <h1>Toast Playground</h1>
-            </header>
+  return (
+    <div className={styles.wrapper}>
+      <header>
+        <img alt="Cute toast mascot" src="/toast.png"/>
+        <h1>Toast Playground</h1>
+      </header>
 
-            <div className={styles.controlsWrapper}>
-                <div className={styles.row}>
-                    <label
-                        htmlFor="message"
-                        className={styles.label}
-                        style={{alignSelf: 'baseline'}}
-                    >
-                        Message
-                    </label>
-                    <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} value={toastMsg} onChange={(e) => setToastMsg(e.target.value)}/>
-                    </div>
-                </div>
+      {showToast ?
+        <Toast icon={ICONS_BY_VARIANT[variant]} className={variant} handleClose={() => setShowToast(false)}>
+          {toastMsg}
+        </Toast>
+        : <></>}
 
-                <div className={styles.row}>
-                    <div className={styles.label}>Variant</div>
-                    <div
-                        className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-                    >
-                        {VARIANT_OPTIONS.map((v) => (
-                            <ToastVariant key={v} variant={v} selected={v === variant} onSelect={handleVariantChange}/>
-                        ))}
-                    </div>
-                </div>
 
-                <div className={styles.row}>
-                    <div className={styles.label}/>
-                    <div
-                        className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-                    >
-                        <Button>Pop Toast!</Button>
-                    </div>
-                </div>
-            </div>
+      <div className={styles.controlsWrapper}>
+        <div className={styles.row}>
+          <label
+            htmlFor="message"
+            className={styles.label}
+            style={{alignSelf: 'baseline'}}
+          >
+            Message
+          </label>
+          <div className={styles.inputWrapper}>
+            <textarea id="message" className={styles.messageInput} value={toastMsg}
+                      onChange={(e) => setToastMsg(e.target.value)}/>
+          </div>
         </div>
-    );
+
+        <div className={styles.row}>
+          <div className={styles.label}>Variant</div>
+          <div
+            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+          >
+            {VARIANT_OPTIONS.map((v) => (
+              <ToastVariant key={v} variant={v} selected={v === variant} onSelect={handleVariantChange}/>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.row}>
+          <div className={styles.label}/>
+          <div
+            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
+          >
+            <Button onClick={() => setShowToast(true)}>Pop Toast!</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 const ToastVariant = ({variant, onSelect, selected}) => {
-    const id = `variant-${variant.toLowerCase()}`;
-    return (
-        <label htmlFor={id}>
-            <input
-                id={id}
-                type="radio"
-                name="variant"
-                value={variant}
-                checked={selected}
-                onChange={onSelect}
-            />
-            {variant}
-        </label>
+  const id = `variant-${variant.toLowerCase()}`;
+  return (
+    <label htmlFor={id}>
+      <input
+        id={id}
+        type="radio"
+        name="variant"
+        value={variant}
+        checked={selected}
+        onChange={onSelect}
+      />
+      {variant}
+    </label>
 
-    )
+  )
 }
 
 export default ToastPlayground;
