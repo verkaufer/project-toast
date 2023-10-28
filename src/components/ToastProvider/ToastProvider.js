f9c59d5d12e0fd3663dfc7c5b009ef232e7d3a8b
David Gunter <david@davidgunter.ca>
2023-10-28 12:48:19 -0700

    Clear toasts when user presses escape key

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -8,6 +8,19 @@ function ToastProvider({children}) {
 
     const [toasts, setToasts] = React.useState([]);
 
+    /** Clear Toasts when user presses Escape key */
+    React.useEffect(() => {
+
+        function handleKeyUp (ev) {
+            // compat: Firefox 36 and earlier maps Escape key to `Esc`
+            if(ev.key === "Escape" || ev.key === "Esc") {
+                setToasts(() => ([]));
+            }
+        }
+        document.addEventListener("keyup", handleKeyUp)
+        return () => (document.removeEventListener("keyup", handleKeyUp))
+    }, [])
+
     const createToast = React.useCallback(({toastMsg, variant = DEFAULT_VARIANT}) => {
         const newToast = {
             id: crypto.randomUUID(),
@@ -25,7 +38,6 @@ function ToastProvider({children}) {
             toasts.filter((t) => t.id !== id)
         ))
     }, [toasts])
-
 
     return <ToastContext.Provider value={{
         toasts,
